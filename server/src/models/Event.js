af9cf5c32da5fb93854de0995bf93d9247fee4b1
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-10-28 10:26:37 -0400

    Add fields to Event schema

--- server/src/models/Event.js
+++ server/src/models/Event.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var Event = new Schema({
-	event: {
-		type: String
-	} 
-}, {
-	collection: 'events'
+	eventName: String,
+	location: String,
+	startDate: String,
+	duration: String,
+	durationUnit: String
 });
 
 module.exports = mongoose.model('Event', Event);
